d27a0e10e641b71062a961542ccd097acdd8d5c9
jerryking <jerryking900000@gmail.com>
2026-05-22 09:00:58 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+__path = process.cwd()
+const bodyParser = require("body-parser");
+const PORT = process.env.PORT || 8000;
+let code = require('./pair'); 
+
+require('events').EventEmitter.defaultMaxListeners = 500;
+
+// ✅ CORS - Vercel pairing site ko allow karo
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    if (req.method === 'OPTIONS') return res.sendStatus(200);
+    next();
+});
+
+app.use('/code', code);
+app.use('/pair', async (req, res, next) => {
+    res.sendFile(__path + '/pair.html')
+});
+app.use('/', async (req, res, next) => {
+    res.sendFile(__path + '/main.html')
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(PORT, () => {
+    console.log(`\nDon't Forget To Give Star ‼️\n\n\nServer running on http://localhost:` + PORT)
+});
+
+module.exports = app;
